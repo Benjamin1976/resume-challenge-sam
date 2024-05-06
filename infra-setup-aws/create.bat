870178acc69fa69d034ca72cce4128d72e74c280
Benjamin <bp_coding@yahoo.com.au>
2024-05-06 23:20:34 +0800

    running whole process clear

---
+++ infra-setup-aws/create.bat
@@ -0,0 +1,3 @@
+sam build -t create.yml && sam deploy -t create.yml
+@REM sam build -t .\children\counter-previous.yml && sam deploy .\children\counter-previous.yml
+@REM sam build -t sports.yml && sam deploy -t sports.yml
---
+++ infra-setup-aws/modify.bat
@@ -0,0 +1,1 @@
+sam build -t modify.yml && sam deploy -t modify.yml
